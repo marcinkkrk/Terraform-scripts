69e221d6a8510c0c633b195d351efbda92f87d92
marcinkkrk <azreak1234@gmail.com>
2023-04-10 22:19:50 +0200

    add exercise 4

---
+++ Nowy folder/vars.tf
@@ -0,0 +1,19 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-06e46074ae430fba6"
+    us-east-2 = "ami-0103f211a154d64a6"
+  }
+}
+
+variable "USER" {
+  default = "ec2-user"
+}
